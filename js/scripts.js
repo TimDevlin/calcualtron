6302409b0fc23d79542e626f009c812c7ca29df0
Rachel Etnire and Tim Devlin <tim@>
2016-09-14 19:28:37 -0700

    add additional functions using Jquery to make Calcualatron a BEAST

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+// Business Logic
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// Front-End Logic
+
+$(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    alert(add(number1, number2));
+  });
+
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    alert(subtract(number1, number2));
+  });
+
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    alert(multiply(number1, number2));
+  });
+
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    alert(divide(number1, number2));
+  });
+
+
+});
